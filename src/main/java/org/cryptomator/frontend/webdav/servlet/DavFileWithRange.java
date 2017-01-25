16fb6f9ceaff9201a7787609d92d485c6ea99a07
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2017-01-25 17:20:28 +0100

    extracted byte range logic into a separate class

--- src/main/java/org/cryptomator/frontend/webdav/servlet/DavFileWithRange.java
+++ src/main/java/org/cryptomator/frontend/webdav/servlet/DavFileWithRange.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Sebastian Stenzel and others.
+ * Copyright (c) 2016, 2017 Sebastian Stenzel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the accompanying LICENSE.txt.
  *
@@ -20,8 +20,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Objects;
 import java.util.Optional;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.jackrabbit.webdav.DavServletResponse;
 import org.apache.jackrabbit.webdav.DavSession;
 import org.apache.jackrabbit.webdav.io.OutputContext;
@@ -37,11 +35,11 @@ import com.google.common.io.ByteStreams;
  */
 class DavFileWithRange extends DavFile {
 
-	private final Pair<String, String> requestRange;
+	private final ByteRange reqRange;
 
-	public DavFileWithRange(DavResourceFactoryImpl factory, LockManager lockManager, DavLocatorImpl locator, Path path, BasicFileAttributes attr, DavSession session, Pair<String, String> requestRange) {
+	public DavFileWithRange(DavResourceFactoryImpl factory, LockManager lockManager, DavLocatorImpl locator, Path path, BasicFileAttributes attr, DavSession session, ByteRange byteRange) {
 		super(factory, lockManager, locator, path, Optional.of(attr), session);
-		this.requestRange = Objects.requireNonNull(requestRange);
+		this.reqRange = Objects.requireNonNull(byteRange);
 	}
 
 	@Override
@@ -52,19 +50,22 @@ class DavFileWithRange extends DavFile {
 			return;
 		}
 		final long contentLength = attr.get().size();
-		final Pair<Long, Long> range = getEffectiveRange(contentLength);
-		if (range.getLeft() < 0 || range.getLeft() > range.getRight() || range.getRight() > contentLength) {
+		final long firstByte = reqRange.getEffectiveFirstByte(contentLength);
+		final long lastByte = reqRange.getEffectiveLastByte(contentLength);
+		final long rangeLength = lastByte - firstByte + 1;
+		assert firstByte >= 0;
+		assert lastByte < contentLength;
+		if (firstByte >= contentLength) {
 			outputContext.setProperty(HttpHeader.CONTENT_RANGE.asString(), "bytes */" + contentLength);
 			throw new UncheckedDavException(DavServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE, "Valid Range would be in [0, " + contentLength + "]");
 		}
-		final Long rangeLength = range.getRight() - range.getLeft() + 1;
 		outputContext.setContentLength(rangeLength);
-		outputContext.setProperty(HttpHeader.CONTENT_RANGE.asString(), contentRangeResponseHeader(range.getLeft(), range.getRight(), contentLength));
+		outputContext.setProperty(HttpHeader.CONTENT_RANGE.asString(), contentRangeResponseHeader(firstByte, lastByte, contentLength));
 		outputContext.setContentType(CONTENT_TYPE_VALUE);
 		outputContext.setProperty(CONTENT_DISPOSITION_HEADER, CONTENT_DISPOSITION_VALUE);
 		outputContext.setProperty(X_CONTENT_TYPE_OPTIONS_HEADER, X_CONTENT_TYPE_OPTIONS_VALUE);
 		try (SeekableByteChannel src = Files.newByteChannel(path, StandardOpenOption.READ); OutputStream out = outputContext.getOutputStream()) {
-			src.position(range.getLeft());
+			src.position(firstByte);
 			InputStream limitedIn = ByteStreams.limit(Channels.newInputStream(src), rangeLength);
 			ByteStreams.copy(limitedIn, out);
 		}
@@ -72,24 +73,6 @@ class DavFileWithRange extends DavFile {
 
 	private String contentRangeResponseHeader(long firstByte, long lastByte, long completeLength) {
 		return String.format("bytes %d-%d/%d", firstByte, lastByte, completeLength);
-	}
-
-	private Pair<Long, Long> getEffectiveRange(long contentLength) {
-		try {
-			final Long lower = requestRange.getLeft().isEmpty() ? null : Long.valueOf(requestRange.getLeft());
-			final Long upper = requestRange.getRight().isEmpty() ? null : Long.valueOf(requestRange.getRight());
-			if (lower == null && upper == null) {
-				return new ImmutablePair<Long, Long>(0l, contentLength - 1);
-			} else if (lower == null) {
-				return new ImmutablePair<Long, Long>(contentLength - upper, contentLength - 1);
-			} else if (upper == null) {
-				return new ImmutablePair<Long, Long>(lower, contentLength - 1);
-			} else {
-				return new ImmutablePair<Long, Long>(lower, Math.min(upper, contentLength - 1));
-			}
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("Invalid byte range: " + requestRange, e);
-		}
 	}
 
 }

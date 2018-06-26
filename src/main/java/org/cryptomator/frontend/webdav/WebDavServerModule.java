d3468d87888546249d52a58bf49ebe3fa2f90581
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2018-06-27 00:16:57 +0200

    organized imports [ci skip]

--- src/main/java/org/cryptomator/frontend/webdav/WebDavServerModule.java
+++ src/main/java/org/cryptomator/frontend/webdav/WebDavServerModule.java
@@ -8,6 +8,20 @@
  *******************************************************************************/
 package org.cryptomator.frontend.webdav;
 
+import dagger.Module;
+import dagger.Provides;
+import org.cryptomator.frontend.webdav.mount.MounterModule;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
+import org.eclipse.jetty.util.thread.ThreadPool;
+
+import javax.inject.Qualifier;
+import javax.inject.Singleton;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -19,23 +33,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import javax.inject.Qualifier;
-import javax.inject.Singleton;
-
-import org.cryptomator.frontend.webdav.mount.MounterModule;
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.thread.ExecutorSizedThreadPool;
-import org.eclipse.jetty.util.thread.ExecutorThreadPool;
-import org.eclipse.jetty.util.thread.ThreadPool;
-
-import dagger.Module;
-import dagger.Provides;
 
 @Module(includes = {MounterModule.class})
 class WebDavServerModule {

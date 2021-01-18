01d994673ba978e97c48b9a23c697d83ceb0b89a
Armin Schrenk <armin.schrenk@skymatic.de>
2021-01-18 15:37:37 +0100

    Add javadoc to UnsupportedSystemException

--- src/main/java/org/cryptomator/frontend/webdav/mount/UnsupportedSystemException.java
+++ src/main/java/org/cryptomator/frontend/webdav/mount/UnsupportedSystemException.java
@@ -2,6 +2,11 @@ package org.cryptomator.frontend.webdav.mount;
 
 import java.net.URI;
 
+/**
+ * Thrown when the underlying OS is not supported mounting a webdav directory.
+ * <p>
+ * It contains the uri of the webdav service to offer callers the possiblilty of process it further.
+ */
 public class UnsupportedSystemException extends Exception {
 
 	private static final String MESSAGE = "No applicable mounting strategy found for this system.";

cb8213f205e4d09dc1889d1da0b85429c4509f70
Armin Schrenk <armin.schrenk@skymatic.de>
2025-09-09 10:52:08 +0200

    add cryptofs test as a test dependency

---
+++ src/test/java/org/cryptomator/frontend/webdav/CryptoFsTest.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Sebastian Stenzel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the accompanying LICENSE.txt.
+ *
+ * Contributors:
+ *     Sebastian Stenzel - initial API and implementation
+ *******************************************************************************/
+package org.cryptomator.frontend.webdav;
+
+import com.google.common.base.Preconditions;
+import org.cryptomator.cryptofs.CryptoFileSystemProperties;
+import org.cryptomator.cryptofs.CryptoFileSystemProvider;
+import org.cryptomator.cryptofs.DirStructure;
+import org.cryptomator.cryptolib.common.MasterkeyFileAccess;
+import org.cryptomator.integrations.mount.MountCapability;
+import org.cryptomator.integrations.mount.MountFailedException;
+import org.cryptomator.integrations.mount.MountService;
+import org.cryptomator.integrations.mount.UnmountFailedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.simple.SimpleLogger;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class CryptoFsTest {
+
+	static {
+		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
+		System.setProperty(SimpleLogger.LOG_FILE_KEY, "System.out");
+		System.setProperty(SimpleLogger.SHOW_DATE_TIME_KEY, "true");
+		System.setProperty(SimpleLogger.DATE_TIME_FORMAT_KEY, "HH:mm:ss:SSS");
+	}
+
+	private static final Logger LOG = LoggerFactory.getLogger(CryptoFsTest.class);
+
+	public static void main(String[] args) throws IOException, MountFailedException, NoSuchAlgorithmException {
+		var webDAVMountService = MountService.get() //
+				.filter(svc -> svc.getClass().getPackageName().startsWith("org.cryptomator.frontend.webdav.")) //
+				.findFirst() //
+				.orElseThrow(() -> new MountFailedException("Did not find a mount provider"));
+		LOG.info("Using mount provider: {}", webDAVMountService.displayName());
+		try (Scanner scanner = new Scanner(System.in)) {
+			LOG.info("Enter path to the vault you want to mirror:");
+			Path vaultPath = Paths.get(scanner.nextLine());
+			Preconditions.checkArgument(CryptoFileSystemProvider.checkDirStructureForVault(vaultPath, "vault.cryptomator", "masterkey.cryptomator") == DirStructure.VAULT, "Not a vault: " + vaultPath);
+
+			LOG.info("Enter vault password:");
+			String passphrase = scanner.nextLine();
+
+			SecureRandom csprng = SecureRandom.getInstanceStrong();
+			CryptoFileSystemProperties props = CryptoFileSystemProperties.cryptoFileSystemProperties()
+					.withKeyLoader(url -> new MasterkeyFileAccess(new byte[0], csprng).load(vaultPath.resolve("masterkey.cryptomator"), passphrase))
+					.build();
+			try (FileSystem cryptoFs = CryptoFileSystemProvider.newFileSystem(vaultPath, props)) {
+				Path p = cryptoFs.getPath("/");
+				mount(webDAVMountService, p, scanner);
+			}
+		}
+	}
+
+	private static void mount(MountService mountProvider, Path pathToMirror, Scanner scanner) throws MountFailedException {
+		var mountBuilder = mountProvider.forFileSystem(pathToMirror);
+		if (mountProvider.hasCapability(MountCapability.VOLUME_ID)) {
+			mountBuilder.setVolumeId("mirror");
+		}
+		if (mountProvider.hasCapability(MountCapability.VOLUME_NAME)) {
+			mountBuilder.setVolumeName("Mirror");
+		}
+		if (mountProvider.hasCapability(MountCapability.FILE_SYSTEM_NAME)) {
+			mountBuilder.setFileSystemName("MirrorFS");
+		}
+		if (! mountProvider.hasCapability(MountCapability.MOUNT_TO_SYSTEM_CHOSEN_PATH)) {
+			// don't set a mount point
+		} else {
+			LOG.info("Enter mount point: ");
+			Path m = Paths.get(scanner.nextLine());
+			mountBuilder.setMountpoint(m);
+		}
+
+		try (var mount = mountBuilder.mount()) {
+			LOG.info("Mounted successfully to: {}", mount.getMountpoint().uri());
+			LOG.info("Enter anything to unmount...");
+			System.in.read();
+
+			try {
+				mount.unmount();
+			} catch (UnmountFailedException e) {
+				if (mountProvider.hasCapability(MountCapability.UNMOUNT_FORCED)) {
+					LOG.warn("Graceful unmount failed. Attempting force-unmount...");
+					mount.unmountForced();
+				}
+			}
+		} catch (UnmountFailedException e) {
+			LOG.warn("Unmount failed.", e);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+}
